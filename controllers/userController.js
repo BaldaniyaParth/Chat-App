9918fec6eade71418bf607f32296cdffc259ec0b
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-02-27 11:07:19 +0530

    Login and Signup page create

---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+const User = require("../models/userModel");
+
+exports.registerLoad = async (req,res) => {
+    try {
+        
+    }catch (err) {
+        res.status.json({
+            status : 500,
+            message : "Something Worng..."
+        })
+    }
+}
+
+exports.register = async (req,res) => {
+    try {
+
+    }catch (err) {
+        res.status.json({
+            status : 500,
+            message : "Something Worng..."
+        })
+    }
+}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,9 @@
 const route = require("express").Router();
-const multer = require("../helper/multer");
+const { uploader } = require("../helper/multer");
+const userController = require("../controllers/userController");
 
-
-
-route.get("/register", );
-route.post("/register", );
+route.get("/register", userController.registerLoad);
+route.post("/register", uploader.single("image"), userController.register);
 
 module.exports = route;
 
