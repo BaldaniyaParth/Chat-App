eae200484fb6780f0ce82250f5e5f41f1baf6a6e
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-02-27 09:55:09 +0530

    userModel Create

---
+++ models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        require : true
+    },
+    email : {
+        type : String,
+        require : true
+    },
+    image : {
+        type : String,
+        require : true
+    },
+    password : {
+        type : String,
+        require : true
+    },
+    is_online : {
+        type : String,
+        default : "0"
+    }
+},
+    { timestamps : true }
+)
+
+module.exports = mongoose.model("User", userSchema);
