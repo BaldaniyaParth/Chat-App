a96cf94d254021a9b12cf9a290abdb1cac8ea19b
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-03-04 16:33:01 +0530

    Chat Application :- chatModel

--- models/chatModel.js
+++ models/chatModel.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
 
+// Define the schema for chat messages
 const chatSchema = new mongoose.Schema({
-    sender_id : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "User"
+    sender_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
     },
-    receiver_id : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "User"
+    receiver_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
     },
-    message : {
-        type : String,
-        require : true
+    message: {
+        type: String,
+        required: true
     }
 },
-    { timestamps : true }
-)
+    // Enable timestamps for createdAt and updatedAt fields
+    { timestamps: true }
+);
+
 
 module.exports = mongoose.model("Chat", chatSchema);
