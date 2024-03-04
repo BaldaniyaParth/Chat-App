33ffd6a5e6be394d1e7fac4c5844c036844bfa3b
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-03-04 16:34:45 +0530

    Chat Application :- userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,28 +1,31 @@
 const mongoose = require("mongoose");
 
+// Define the schema for users
 const userSchema = new mongoose.Schema({
-    name : {
-        type : String,
-        require : true
+    name: {
+        type: String,
+        required: true
     },
-    email : {
-        type : String,
-        require : true
+    email: {
+        type: String,
+        required: true
     },
-    password : {
-        type : String,
-        require : true
+    password: {
+        type: String,
+        required: true
     },
-    image : {
-        type : String,
-        require : true
+    image: {
+        type: String,
+        required: true
     },
-    is_online : {
-        type : String,
-        default : "0"
+    is_online: {
+        type: String,
+        default: "0"
     }
 },
-    { timestamps : true }
-)
+    // Enable timestamps for createdAt and updatedAt fields
+    { timestamps: true }
+);
+
 
 module.exports = mongoose.model("User", userSchema);
