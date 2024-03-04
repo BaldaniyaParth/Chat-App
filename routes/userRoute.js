21b41df21367544774f387e508569b0bf27c25b4
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-03-04 16:54:34 +0530

    Chat Application :- userRoute

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,20 +1,27 @@
-const route = require("express").Router();
+const express = require("express");
+const router = express.Router();
 const { uploader } = require("../helper/multer");
 const userController = require("../controllers/userController");
 const { isLogin, isLogout } = require("../middlewares/userAuth");
 
+// Route to load login page
+router.get("/", isLogout, userController.loginLoad);
 
+// Route to handle user login
+router.post("/login", userController.login);
 
-route.get("/", isLogout, userController.loginLoad);
-route.post("/login", userController.login);
-route.post("/register", uploader.single("image"), userController.register);
-route.get("/logout", isLogin, userController.logout);
-route.get("/dashboard", isLogin, userController.dashboardLoad);
+// Route to handle user registration
+router.post("/register", uploader.single("image"), userController.register);
 
+// Route to handle user logout
+router.get("/logout", isLogin, userController.logout);
 
-route.get("*", (req,res) => {
+// Route to load dashboard page
+router.get("/dashboard", isLogin, userController.dashboardLoad);
+
+// Catch-all route for invalid URLs
+router.get("*", (req, res) => {
     res.redirect("/");
-})
-module.exports = route;
+});
 
-
+module.exports = router;
