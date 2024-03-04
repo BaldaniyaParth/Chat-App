4c29c4b243094edecd66c901a6463a92c46253e2
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-03-04 16:22:33 +0530

    Chat Application :- userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,109 +2,114 @@ const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const cloudinary = require("../helper/cloudinary");
 
+// Controller function to render login page
 exports.loginLoad = async (req, res) => {
     try {
+        // Render the register page
         res.render("register.ejs");
 
-    }catch (err) {
+    } catch (err) {
         return res.status(500).json({
-            status : 500,
-            message : "Something Worng..."
-        })
+            status: 500,
+            message: "Something Worng..."
+        });
     }
 }
 
-exports.register = async (req,res) => {
+// Controller function to register a new user
+exports.register = async (req, res) => {
     try {
-
+        // Hash the password
         const passwordHash = await bcrypt.hash(req.body.password, 10);
 
+        // Upload profile picture to cloudinary
         const profile_pic = req.file;
         const result = await cloudinary.uploader.upload(profile_pic.path);
         const profilePicUrl = result.secure_url;
 
+        // Create a new user instance with hashed password and profile picture URL
         const newUser = new User({
-            name : req.body.name,
-            email : req.body.email,
-            password : passwordHash,
-            image : profilePicUrl
-        })
+            name: req.body.name,
+            email: req.body.email,
+            password: passwordHash,
+            image: profilePicUrl
+        });
 
         await newUser.save();
 
+        // Redirect to home page
         return res.redirect("/");
 
-    }catch (err) {
+    } catch (err) {
         console.log(err);
         return res.status(500).json({
-            status : 500,
-            message : "Something Worng..."
-        })
+            status: 500,
+            message: "Something Worng..."
+        });
     }
 }
 
+// Controller function to handle user login
 exports.login = async (req, res) => {
     try {
         const { email, password } = req.body;
         
-        const userData = await User.findOne({email});
+        const userData = await User.findOne({ email });
 
-        if(userData){
-
+        if (userData) {
+            // Compare passwords
             const passwordMatch = await bcrypt.compare(password, userData.password);
 
-            if(passwordMatch){
-
+            if (passwordMatch) {
+                // Store user data in session and redirect to dashboard
                 req.session.user = userData;
                 res.cookie("user", JSON.stringify(userData));
-                res.redirect("/dashboard");
+                return res.redirect("/dashboard");
+            } else {
 
-            }else{
-
-                res.redirect("/");
-
+                return res.redirect("/");
             }
-        }else{
+        } else {
 
-            res.redirect("/");
-
+            return res.redirect("/");
         }
-    }catch (err) {
+    } catch (err) {
         return res.status(500).json({
-            status : 500,
-            message : "Something Worng..."
-        })
+            status: 500,
+            message: "Something Worng..."
+        });
     }
 }
 
+// Controller function to handle user logout
 exports.logout = async (req, res) => {
     try {
-
+        // Destroy session and clear user cookie, then redirect to home page
         req.session.destroy();
         res.clearCookie("user");
         res.redirect("/");
+
+    } catch (err) {
         
-    }catch (err) {
         return res.status(500).json({
-            status : 500,
-            message : "Something Worng..."
-        })
+            status: 500,
+            message: "Something Worng..."
+        });
     }
 }
 
+// Controller function to render dashboard page
 exports.dashboardLoad = async (req, res) => {
     try {
+        // Find all users except the currently logged in user
+        const users = await User.find({ _id: { $nin: [req.session.user._id] } });
 
-        const users = await User.find({ _id : { $nin : [req.session.user._id]}});
+        res.render("dashboard.ejs", { user: req.session.user, users: users });
 
-        res.render("dashboard.ejs", {user : req.session.user, users : users});
-
-    }catch (err) {
+    } catch (err) {
         return res.status(500).json({
-            status : 500,
-            message : "Something Worng..."
-        })
+            status: 500,
+            message: "Something Worng..."
+        });
     }
 }
-
-
