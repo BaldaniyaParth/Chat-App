de16759937b5501258abb01a8fcc14e8bdd504c9
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-03-04 16:25:10 +0530

    Chat Application :- Database Connection

--- db/connect.js
+++ db/connect.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+// Connect to MongoDB using the provided URL from environment variables
 mongoose
     .connect(process.env.MONGODB_URL)
-    .then( () => {
+    .then(() => {
+        
         console.log("Connection Succesfulyy...");
     })
-    .catch( (err) => {
+    .catch((err) => {
+        
         console.log("error : ", err);
-    })
+    });
