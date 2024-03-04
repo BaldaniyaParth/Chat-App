df88169dc4a1f95b11860ea875ee03af7184ce82
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-03-04 16:31:24 +0530

    Chat Application :- userAuth - Authentication login or logout

--- middlewares/userAuth.js
+++ middlewares/userAuth.js
@@ -1,30 +1,32 @@
-const isLogin = async (req, res, next) => {
+// Middleware to check if user is logged in
+exports.isLogin = async (req, res, next) => {
     try {
+        // If user is not logged in, redirect to home page
         if (!req.session.user) {
             res.redirect("/");
             return; 
         }
 
+        // If user is logged in, proceed to next middleware or route handler
         next();
     } catch (err) {
         console.log("error : ", err);
     }
 }
 
-const isLogout = async (req, res, next) => {
+
+// Middleware to check if user is logged out
+exports.isLogout = async (req, res, next) => {
     try {
+        // If user is logged in, redirect to dashboard
         if (req.session.user) {
             res.redirect("/dashboard");
             return; 
         }
 
+        // If user is logged out, proceed to next middleware or route handler
         next();
     } catch (err) {
         console.log("error : ", err);
     }
-}
-
-module.exports = { 
-    isLogin,
-    isLogout
 }
