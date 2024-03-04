7908d7996785c016b8a99a755666d4921be70b1b
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-03-04 16:18:52 +0530

    Chat Application :- chatController

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,70 +1,69 @@
 const Chat = require("../models/chatModel");
 
-exports.saveChat = async (req,res) => {
+// Controller function to save a chat message
+exports.saveChat = async (req, res) => {
     try {
-
-        const chat =  new Chat({
-            sender_id : req.body.sender_id,
-            receiver_id : req.body.receiver_id,
-            message : req.body.message
-        })
+        // Create a new chat instance with data from the request body
+        const chat = new Chat({
+            sender_id: req.body.sender_id,
+            receiver_id: req.body.receiver_id,
+            message: req.body.message
+        });
 
         const newChat = await chat.save();
 
         return res.status(200).send({
-            success : true,
-            message : "Chat Insert Successfully...",
-            data : newChat
-        })
+            success: true,
+            message: "Chat Insert Successfully...",
+            data: newChat
+        });
 
-    }catch (err) {
+    } catch (err) {
         return res.status(500).json({
-            status : 500,
+            status: 500,
             message: "Something Worng..."
-        })
+        });
     }
-}
+};
 
-exports.deleteChat = async (req,res) => {
+// Controller function to delete a chat message
+exports.deleteChat = async (req, res) => {
     try {
-        
-        await Chat.deleteOne( { _id : req.body.id });
+        // Delete the chat message using its ID
+        await Chat.deleteOne({ _id: req.body.id });
 
         return res.status(200).send({
-            success : true,
-            message : "Chat Delete Successfully..."
-        })
+            success: true,
+            message: "Chat Delete Successfully..."
+        });
 
-    }catch (err) {
+    } catch (err) {
         return res.status(500).json({
-            status : 500,
+            status: 500,
             message: "Something Worng..."
-        })
+        });
     }
-}
+};
 
-exports.updateChat = async (req,res) => {
+// Controller function to update a chat message
+exports.updateChat = async (req, res) => {
     try {
-        
-        await Chat.findByIdAndUpdate( { _id : req.body.id },{
-            $set : {
-                message : req.body.message
+        // Update the chat message using its ID and set the new message
+        await Chat.findByIdAndUpdate({ _id: req.body.id }, {
+            $set: {
+                message: req.body.message
             }
         });
 
         return res.status(200).send({
-            success : true,
-            message : "Chat Update Successfully..."
-        })
+            success: true,
+            message: "Chat Update Successfully..."
+        });
 
-    }catch (err) {
+    } catch (err) {
         return res.status(500).json({
-            status : 500,
+            status: 500,
             message: "Something Worng..."
-        })
+        });
     }
-}
-
-
-
-
+};
