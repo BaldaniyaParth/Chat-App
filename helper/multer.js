2f84ccb914984982727978865133afd43bd4cc26
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-03-04 16:27:48 +0530

    Chat Application :- multer file - Image Upload

--- helper/multer.js
+++ helper/multer.js
@@ -1,7 +1,9 @@
 const multer = require("multer");
 
+// Multer middleware for file uploading
 exports.uploader = multer({
-    storage : multer.diskStorage({}),
-    limits : {fileSize : 5000000} 
-})
-
+    
+    storage: multer.diskStorage({}),
+    
+    limits: { fileSize: 5000000 } // Set file size limit to 5MB
+});
